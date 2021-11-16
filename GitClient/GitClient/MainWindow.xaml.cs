90257d6ffe1eb98772dbf337a5c7774017d78814
Quofite <gleb.n1kit1n@yandex.ru>
2021-11-16 21:23:28 +0300

    сделан пуш в gh, сохранение ссылки пока не работает

--- GitClient/GitClient/MainWindow.xaml.cs
+++ GitClient/GitClient/MainWindow.xaml.cs
@@ -1,12 +1,35 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace GitClient {
+    class JsonLink {
+        public string Link { get; set; }
+    }
+
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private async Task setInJson(string workingDir, string gitlink) {
+            using (FileStream fs = new FileStream("gitlink.json", FileMode.OpenOrCreate)) {
+                JsonLink link = new JsonLink() { Link = gitlink };
+                await JsonSerializer.SerializeAsync<JsonLink>(fs, link);
+                Thread.Sleep(5000);
+                File.Move("./gitlink.json", dirTextBox.Text + @"\gitlink.json");
+            }
+        }
+
+        private async Task<string> getFromJson(string workingDir) {
+            using (FileStream fs = new FileStream(workingDir + "gitlink.json", FileMode.OpenOrCreate)) {
+                JsonLink link = await JsonSerializer.DeserializeAsync<JsonLink>(fs);
+                return link.Link;
+            }
         }
 
         private void openDirBtn_Click(object sender, RoutedEventArgs e) {
@@ -16,8 +39,12 @@ namespace GitClient {
 
 
             if (Directory.Exists(workingDir)) {
-                if (Directory.Exists(workingDir + @"\.git"))
+                if (Directory.Exists(workingDir + @"\.git")) {
                     gitStatusBar.Text += "git initialized";
+
+                    if (File.Exists(workingDir + @"\gitlink.json"))
+                        gitLink.Text = getFromJson(workingDir).ToString();
+                }
                 else {
                     File.WriteAllText("./gitinitscript.bat",
                         "cd " + workingDir + "\n" +
@@ -33,14 +60,14 @@ namespace GitClient {
                 string[] dirs = Directory.GetDirectories(workingDir);
                 foreach (string dir in dirs) {
                     string[] splited = dir.Split("\\");
-                    CheckBox checkbox = new CheckBox { Content = splited[splited.Length - 1], FontSize = 16, Height = 20 };
+                    CheckBox checkbox = new CheckBox { Content = splited[splited.Length - 1], FontSize = 16, Height = 20, Margin = new Thickness(10, 10, 0, 0) };
                     stackPanel.Children.Add(checkbox);
                 }
 
                 string[] files = Directory.GetFiles(workingDir);
                 foreach (string file in files) {
                     string[] splited = file.Split("\\");
-                    CheckBox checkbox = new CheckBox { Content = splited[splited.Length - 1], FontSize = 16, Height = 20 };
+                    CheckBox checkbox = new CheckBox { Content = splited[splited.Length - 1], FontSize = 16, Height = 20, Margin = new Thickness(10, 10, 0, 0) };
                     stackPanel.Children.Add(checkbox);
                 }
             }
@@ -62,6 +89,27 @@ namespace GitClient {
             batnik.Delete();
         }
 
+
+        private void gitPushBtn_Click(object sender, RoutedEventArgs e) {
+            if (gitLink.Text == string.Empty) {
+                MessageBox.Show("Нет ссылки на репозиторий");
+                return;
+            }
+
+            File.WriteAllText("./gitpush.bat", $"cd {dirTextBox.Text} \n" + $"git remote add origin {gitLink.Text} \n" + "git push -u origin master");
+            Process.Start("./gitpush.bat");
+            MessageBox.Show("Файлы успешно загруженны на удаленный репозиторий");
+            FileInfo batnik = new FileInfo("./gitpush.bat");
+            batnik.Delete();
+        }
+
+
+        private void saveLinkBtn_Click(object sender, RoutedEventArgs e) {
+            setInJson(dirTextBox.Text, gitLink.Text);            
+        }
+
+
+        // плейсхолдеры для текстовых полей
         private void comment_GotFocus(object sender, RoutedEventArgs e) {
             if (comment.Text == "Комментарий")
                 comment.Text = string.Empty;
@@ -70,6 +118,16 @@ namespace GitClient {
         private void comment_LostFocus(object sender, RoutedEventArgs e) {
             if (string.IsNullOrWhiteSpace(comment.Text))
                 comment.Text = "Комментарий";
+        }
+
+        private void gitLink_GotFocus(object sender, RoutedEventArgs e) {
+            if (gitLink.Text == "Ссылка на удаленный репозиторий")
+                gitLink.Text = string.Empty;
+        }
+
+        private void gitLink_LostFocus(object sender, RoutedEventArgs e) {
+            if (string.IsNullOrWhiteSpace(gitLink.Text))
+                gitLink.Text = "Ссылка на удаленный репозиторий";
         }
     }
 }
