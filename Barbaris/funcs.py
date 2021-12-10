ea9be6a9694e4890e3e4cc20f26688947d53c517
Quofite <gleb.n1kit1n@yandex.ru>
2021-12-10 22:20:00 +0300

    читай ченджлог от 10.12.2021

---
+++ Barbaris/funcs.py
@@ -0,0 +1,66 @@
+import os
+import csv
+
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
+
+
+def make_popup(header, text):
+    mbox = QMessageBox()
+    mbox.setWindowTitle(header)
+    mbox.setText(text)
+    mbox.setStandardButtons(QMessageBox.Close)
+    mbox.exec_()
+
+
+# ---------------------------------------- КНОПКИ НА ПРАВОЙ ПАНЕЛИ
+def open_ggc():
+    try:
+        os.startfile(r'C:\Users\marga\Desktop\Barbaris\GitClient.exe')
+    except Exception:
+        make_popup("Ошибка", "Файл GitClient.exe не найден в корневой папке Barbaris")
+
+
+def open_autosaver():
+    make_popup("Недоступно", "Это приложение еще не создано")
+
+
+def open_vsc():
+    try:
+        os.startfile(r'C:\CodingUtilities\Microsoft VS Code\Code.exe')
+    except Exception:
+        make_popup("Ошибка", "Visual Studio Code не найден на вашем ПК, установите его")
+
+
+def open_calc():
+    try:
+        os.startfile(r'C:\Users\marga\Desktop\Barbaris\Calculator.exe')
+    except Exception:
+        make_popup("Ошибка", "Calculator.exe не найден в корневой папке Barbaris")
+
+
+def open_convertor():
+    make_popup("Недоступно", "Это приложение еще не создано")
+
+
+# ------------------------------------------ ВСЕ, СВЯЗАННОЕ С ПРОЕКТАМИ
+def add_proj(main_window):
+    file = str(QFileDialog.getExistingDirectory(main_window, "Выберете директорию"))
+    splitted = file.split('/')
+    projname = splitted[len(splitted) - 1]
+    proj = [projname, file]
+
+    with open("projects.csv", 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(proj)
+
+
+def show_projs(vbox):
+    with open("projects.csv", 'r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row[0] + " - " + row[1])
+
+
+# ------------------------------------------ КОНФИГУРАТОР
+def save_config():
+    return
--- Launcher/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.launcher.launcher {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.launcher.launcher to javafx.fxml;
-    exports com.launcher.launcher;
-}
