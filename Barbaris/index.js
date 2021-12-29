f8b8f26960a510122a0f4688a391772468da8d35
Doogls <alex2066439@gmail.com>
2021-12-29 03:01:51 +0300

    Трэй меню

--- Barbaris/index.js
+++ Barbaris/index.js
@@ -1,25 +1,75 @@
 const path = require("path");
 const url = require("url");
-const {app, BrowserWindow} = require("electron");
+const {
+    app, 
+    BrowserWindow,
+    Tray,
+    Menu
+} = require("electron");
 
+var mainWindow;
 app.on("ready", () => {
-    let win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 950,
         height: 600,
+        title: "Barbaris",
         icon: "Barbaris.ico",
         minWidth: 750,
         minHeight: 450
     });
 
-    win.loadURL(url.format({
+    mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "index.html"),
         protocol: "file",
         slashes: true
     }));
 
-    //win.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
-    win.on("closed", () => { win = null; });
+    mainWindow.on("minimize", (event) => {
+        event.preventDefault();
+
+        newTray();
+
+        mainWindow.hide();
+    });
+
+    mainWindow.on("close", (event) => { 
+        if (!app.isQuiting){
+            event.preventDefault();
+
+            newTray();
+
+            mainWindow.hide();
+        }
+    
+        return false;
+    });
 });
 
-app.on("window-all-closed", ()=> { app.quit(); });
+const newTray = () => {
+    let tray = new Tray("Barbaris.ico")
+    tray.setToolTip("Barbaris")
+    tray.setContextMenu(Menu.buildFromTemplate([
+        { 
+            label: "Open Barbaris",
+            click: () => {
+                mainWindow.show();
+            }
+        },
+        { 
+            label: "Close",
+            click: () => {
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ]))
+}
+
+/*app.whenReady().then(() => {
+})*/
+
+app.on("window-all-closed", () => {
+    app.quit(); 
+});
