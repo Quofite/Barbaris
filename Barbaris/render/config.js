52404fbfac1401663e8eaf936493659753d41854
Doogls <alex2066439@gmail.com>
2022-01-15 15:49:57 +0300

    Проверки на говно и вывод кнопок

--- Barbaris/render/config.js
+++ Barbaris/render/config.js
@@ -1,29 +1,39 @@
-var { ipcRenderer } = require("electron");
-
-/*document.querySelector("#save").addEventListener("click", () => {
-	let idePath = document.getElementById("idePathField").value;
-	let ggcPath = document.getElementById("gitPathField").value;
-	let convPath = document.getElementById("converterPathField").value;
-	let backuperPath = document.getElementById("backuperPathField").value;
-
-	let pathes = JSON.stringify({
-		ide: idePath,
-		ggc: ggcPath,
-		convertor: convPath,
-		backuper: backuperPath
-	});
-
-	console.log(pathes);
-
-	require("fs").writeFile("pathes.json", pathes, (error) => {
-		if(error) throw error;
-	});
-
-	ipcRenderer.send("saved");
-});*/
+var { 
+	ipcRenderer 
+} = require("electron");
 
 document.querySelector("#addProgram").addEventListener("click", () => {
-	let programs = JSON.parse(require("fs").readFileSync("pathes.json", "utf8"));
+	let programs = JSON.parse((!require("fs").readFileSync("pathes.json", "utf8")) ? "[]" : require("fs").readFileSync("pathes.json", "utf8"));
+	if (!programs) {
+		programs = [
+			{
+				"path": document.getElementById("idePath").files[0].path,
+				"name": "IDE"
+			}
+		]
+	}
+
+	if (programs[0] != null && programs[0].name != "IDE") {
+		programs[1] = programs[0];
+		programs[0] = {
+			"path": document.getElementById("idePath").files[0].path,
+			"name": "IDE"
+		}
+	}
+
+	programs[0] = {
+		"path": (document.getElementById("idePath").files.length <= 0) ? (!programs[0]) ? "" : programs[0].path : document.getElementById("idePath").files.length <= 0,
+		"name": "IDE"
+	}
+
+	if (document.getElementById("addProgramPath").files.length <= 0) {
+		require("fs").writeFile("pathes.json", JSON.stringify(programs), (error) => {
+			if (error) 
+				throw error;
+		});
+
+		return
+	}
 
 	programs.push({
 		"path": document.getElementById("addProgramPath").files[0].path,
--- Barbaris/render/main.js
+++ Barbaris/render/main.js
@@ -20,73 +20,50 @@ for (let j = 0; j < parsedPath.length - 2; j++) {
     pathToExecFile += parsedPath[j] + "\\";
 }
 
-// --------------------------------------------------------------Кнопки слева
-// Открыть IDE
-/*document.querySelector("#openIDE").addEventListener("click", () => {
-    var exec = require("child_process").execFile;
+// Добавление и логика левых кнопочек, нужно обрабатывать при onload
+const leftButtons = () => {
     const fs = require("fs");
 
-    let pathesJsonContent = fs.readFileSync("pathes.json", "utf8");
-    let parsed = JSON.parse(pathesJsonContent);
+    let programs = JSON.parse((!require("fs").readFileSync("pathes.json", "utf8")) ? "[]" : require("fs").readFileSync("pathes.json", "utf8"));
+    programs.forEach((program, i, array) => {
+        /*var element = document.createElement("div");
+        element.setAttribute("class", "card");*/
 
-    exec(parsed.ide, function(err, data) {  
-        console.log(err)                      
+        document.getElementById("programsButtons").innerHTML = mustache.render(
+            fs.readFileSync("render/templates/program.html", "utf-8"), 
+            { 
+                name: program.name,
+                path: program.path,
+                icon: program.icon
+            }
+        ) + document.getElementById("programsButtons").innerHTML;
     });
-});
 
-// Открыть GGC
-document.querySelector("#openGit").addEventListener("click", () => {
-    var exec = require("child_process").execFile;
+    if (!programs[0])
+        return
 
-    let pathesJsonContent = require("fs").readFileSync("pathes.json", "utf8");
-    let parsed = JSON.parse(pathesJsonContent);
-
-    if(parsed.ggc === "GitClient.exe"){
-        exec(pathToExecFile + parsed.ggc, function(err, data) {  
-            console.log(err)                    
-        });
-    }
-    else {
-        exec(parsed.ggc, function(err, data) {  
-            console.log(err)                    
-        });
-    }
-});*/
+    document.getElementById("programButton").addEventListener("click", (event) => { // Не робит почему-то
+        console.log(event.target.dataset.path);
+        require("child_process").execFile(event.target.dataset.path);
+    })
+}
 
 // Открыть конфиг
 document.querySelector("#openConfig").addEventListener("click", () => {
     ipcRenderer.send("openConfig");
 });
 
-// Открыть конфиг
+// Скрыть окно
 document.querySelector("#minimize").addEventListener("click", () => {
     ipcRenderer.send("minimize");
 });
 
-// Открыть конфиг
+// Увеличить окно во всеь экран
 document.querySelector("#maximize").addEventListener("click", () => {
     ipcRenderer.send("maximize");
 });
 
-// Открыть конвертор
-/*document.querySelector("#openConverter").addEventListener("click", () => {
-    var exec = require("child_process").execFile;
-
-    let pathesJsonContent = require("fs").readFileSync("pathes.json", "utf8");
-    let parsed = JSON.parse(pathesJsonContent);
-
-    if(parsed.convertor === "convertor\\Convertor.exe"){
-        exec(pathToExecFile + parsed.convertor, function(err, data) {  
-            console.log(err)                    
-        });
-    }
-    else {
-        exec(parsed.convertor, function(err, data) {  
-            console.log(err)                    
-        });
-    }
-});*/
-
+// Документация
 document.querySelector("#openDoc").addEventListener("click", () => {
     require("child_process").exec(`start "" "` + "https://github.com/Quofite/Barbaris#readme" + `"`);
 });
@@ -100,9 +77,7 @@ document.querySelector("#newProjBtn").addEventListener("click", (e) => {
 
 ipcRenderer.on("got-directory-for-new", (e, data) => {
     let projects = JSON.parse(require("fs").readFileSync("projects.json", "utf8"));
-
-    console.log(data)
-
+    
     projects.push({
 		"path": data[0],
 		"name": "",
@@ -119,6 +94,8 @@ ipcRenderer.on("got-directory-for-new", (e, data) => {
 
 // ------------------------------------------------ загрузка проектов из памяти
 function showProjects(){
+    leftButtons();
+
     const fs = require("fs");
     //var parser = require('csv-parser');
     var mainBlock = document.getElementById("mainBlock");
@@ -128,9 +105,6 @@ function showProjects(){
 
     let projects = JSON.parse(fs.readFileSync("projects.json", "utf8"));
     projects.forEach((project, i, array) => {
-        /*var element = document.createElement("div");
-        element.setAttribute("class", "card");*/
-
         document.getElementById("mainBlock").innerHTML = mustache.render(
             fs.readFileSync("render/templates/project.html", "utf-8"), 
             { 
@@ -139,85 +113,15 @@ function showProjects(){
                 description: project.description
             }
         );
-        //document.getElementById("mainBlock").innerHTML = rendered;
-
-        /*var header = document.createElement("header");
-        var projName = document.createTextNode(item.name);
-        header.appendChild(projName);
-        var path = document.createElement("span");
-        path.setAttribute("class", "grey");
-        var pathText = document.createTextNode(item.path);
-        path.appendChild(pathText);
-        header.appendChild(path);
-        element.appendChild(header);
-
-        var contentbar = document.createElement("content");
-        var contentBarContent = document.createTextNode("Проект " + item.name);
-        contentbar.appendChild(contentBarContent);
-        element.appendChild(contentbar);
-
-        var footer = document.createElement("footer");
-
-        var openProjFolderBtn = document.createElement("a");
-        openProjFolderBtn.setAttribute("id", "openProjectFolder");
-        openProjFolderBtn.setAttribute("data-folder", item.path);
-        openProjFolderBtn.setAttribute("href", "#");
-        var folderIcon = document.createElement("i");
-        folderIcon.setAttribute("class", "bi bi-folder-symlink");
-        openProjFolderBtn.appendChild(folderIcon);
-        var infoText1 = document.createTextNode("Открыть папку проекта");
-        openProjFolderBtn.appendChild(infoText1);
-
-        var openProjIdeBtn = document.createElement("a");
-        openProjIdeBtn.setAttribute("id", "openProjectFolder");
-        openProjIdeBtn.setAttribute("data-folder", item.path);
-        openProjIdeBtn.setAttribute("href", "#");
-        var ideIcon = document.createElement("i");
-        ideIcon.setAttribute("class", "bi bi-box-arrow-up-left");
-        openProjIdeBtn.appendChild(ideIcon);
-        var infoText2 = document.createTextNode("Открыть проект в IDE");
-        openProjIdeBtn.appendChild(infoText2);
-
-        var deleteProjBtn = document.createElement("a");
-        deleteProjBtn.setAttribute("id", "deleteProject");
-        deleteProjBtn.setAttribute("data-folder", i);
-        deleteProjBtn.setAttribute("href", "#");
-        var delIcon = document.createElement("i");
-        delIcon.setAttribute("class", "bi bi-folder-minus");
-        deleteProjBtn.appendChild(delIcon);
-        var infoText3 = document.createTextNode("Удалить проект");
-        deleteProjBtn.appendChild(infoText3);*/
 
         document.getElementById("openProjectFolder").addEventListener("click", (event) => {
             require("child_process").exec(`start "" "` + event.target.dataset.folder + `"`);
         });
 
         document.getElementById("openProjectIDE").addEventListener("click", (event) => {
-            var exec = require("child_process").execFile;
-            
-            exec(JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).ide, [event.target.dataset.folder], function(err, data) {  
+            require("child_process").execFile(JSON.parse(require("fs").readFileSync("pathes.json", "utf8"))[0].path, [event.target.dataset.folder], function(err, data) {  
                 console.log(err)                    
             });
-
-            if (JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).ggc === "GitClient.exe"){
-                exec(pathToExecFile + JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).ggc, [event.target.dataset.folder], function(err, data) {  
-                    console.log(err)                    
-                });
-            } else {
-                exec(JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).ggc, [event.target.dataset.folder], function(err, data) {  
-                    console.log(err)                    
-                });
-            }
-
-            if (JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).backuper === "Backuper.exe"){
-                exec(pathToExecFile + JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).ggc, [event.target.dataset.folder], function(err, data) {  
-                    console.log(err)                    
-                });
-            } else{
-                exec(JSON.parse(require("fs").readFileSync("pathes.json", "utf8")).backuper, [event.target.dataset.folder], function(err, data) {  
-                    console.log(err)                    
-                });
-            }
         });
 
         document.getElementById("deleteProject").addEventListener("click", (event) => {
@@ -230,11 +134,5 @@ function showProjects(){
 
             showProjects();
         });
-
-        /*footer.appendChild(openProjFolderBtn);
-        footer.appendChild(openProjIdeBtn);
-        footer.appendChild(deleteProjBtn);
-        element.appendChild(footer);
-        mainBlock.appendChild(element);*/
     });
 }
